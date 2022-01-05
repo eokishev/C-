4d407429a1bf39e2db00d30f0a744bfc56ad0d38
Evgeny Okishev <e.okishev@lcgroup.su>
2022-01-05 17:13:30 +0700

    Переменные, ввод вывод

---
+++ .vs/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите число");
+            string x = Console.ReadLine();
+            int a = Convert.ToInt32(x);
+            a = a * a;
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
